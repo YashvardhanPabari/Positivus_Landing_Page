c6fcec7f3f5ae732289484b22c4ece64f26b15a8
Yashvardhan <yashwardhanpabari@gmail.com>
2025-02-25 16:16:48 +0530

    Ensure logo.png is added

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Logo from "../../assets/logo.png";
+import Logo from "../../assets/Logo.png";
 import { Menu, X } from "lucide-react";
 import { Link } from "react-scroll";
 
